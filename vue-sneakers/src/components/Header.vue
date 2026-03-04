7a80f514734c7ee1e604611fc13de8743b62c724
DmitriySokolov2023 <sokolovoff2023@gmail.com>
2026-03-04 20:50:57 +0300

    fix: address PR comments

--- vue-sneakers/src/components/Header.vue
+++ vue-sneakers/src/components/Header.vue
@@ -5,7 +5,7 @@
 			<div>
 				<h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
 				<p class="text-slate-400">Магазин лучших кроссовок</p>
-				<p>Добро пожаловать!</p>
+				<p>Добро пожаловать!!!!</p>
 			</div>
 		</div>
 
